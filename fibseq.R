69d09f23a2eec8569e6870b1cd0a4fa51af84eea
Gornstats <railspider@gmail.com>
2015-07-29 01:03:55 +1000

    initial commit
    simple fibonacci function

---
+++ fibseq.R
@@ -0,0 +1,13 @@
+# My iterative fibonacci function
+# Generates sequence of fibonacci numbers 'x' units long
+fibseq = function(x){
+  if(x>10) print("Sequence too large. Use smaller integer")
+  else if(x<3) print("Input too small. Use integer larger than 3")
+  else {
+    fib = c(1,1)
+    #next number in sequence is sum of previous two
+    for(i in seq(x-2)) fib = append(fib,sum(tail(fib,n=2)))
+    return(fib)
+  }
+}
+
