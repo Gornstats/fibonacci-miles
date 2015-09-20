5285f5fbd3503112b6ac226cd329b757bef29dfd
Gornstats <railspider@gmail.com>
2015-09-20 14:37:24 +1000

    data type checking for fibseq

--- fibseq.R
+++ fibseq.R
@@ -1,13 +1,16 @@
 # Iterative fibonacci function
 # Generates sequence of fibonacci numbers 'x' units long
 fibseq = function(x){
-  if(x>50) print("Sequence too large. Use smaller integer")
-  else if(x<3) print("Input too small. Use integer larger than 3")
-  else {
-    fib = c(1,1)
-    #next number in sequence is sum of previous two
-    for(i in seq(x-2)) fib = append(fib,sum(tail(fib,n=2)))
-    return(fib)
+  if(typeof(x) != "double") print("Please enter a number between 3 and 50")
+  else{
+    if(x>50) print("Sequence too large. Use smaller integer")
+    else if(x<3) print("Input too small. Use integer larger than 3")
+    else {
+      fib = c(1,1)
+      #next number in sequence is sum of previous two
+      for(i in seq(x-2)) fib = append(fib,sum(tail(fib,n=2)))
+      return(fib)
+    }
   }
 }
 
