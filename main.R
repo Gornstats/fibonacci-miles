2789c92bd21c1cfa5792594e56460118e6f5548f
Gornstats <railspider@gmail.com>
2015-09-20 15:23:45 +1000

    differences calculated and charted

--- main.R
+++ main.R
@@ -4,10 +4,28 @@ source("fibseq.R")
 goldenRatio = (1 + sqrt(5))/2    # roughly 1.618
 kmMiles = 1.609344    # kilometres in one mile
 
-# TODO
-# create vector of fibonacci ratios
-# calculate difference between fib-ratios and:
-# 1. golden ratio
-# 2. kilometres in a mile
+fibs = fibseq(50)
+fibRatios = c()
+for (i in 2:length(fibs)) {
+  fibRatios = append(fibRatios, fibs[i]/fibs[i-1])
+}
 
-# Chart absolute difference('error')
+plot(fibRatios)
+abline(h=goldenRatio)
+# Notice ratio very quickly tends to golden ratio
+# Identical to 6 decimal places at 17th fib number
+# Asymptotically approaching forever
+goldenDiff = abs(fibRatios-goldenRatio)
+milesDiff = abs(fibRatios-kmMiles)
+
+plot(goldenDiff, xlab="Fibonacci number", ylab="Absolute Difference", 
+     main="Difference between Fibonacci ratio and Golden Ratio")
+
+plot(milesDiff, xlab="Fibonacci number", ylab="Absolute Difference", 
+     main="Difference between Fibonacci ratio and Miles/KM ratio")
+
+# save an image
+png(filename="goldenRatioChart.png")
+plot(goldenDiff, xlab="Fibonacci number", ylab="Absolute Difference", 
+     main="Difference between Fibonacci ratio and Golden Ratio")
+dev.off()
